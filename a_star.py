df057dbdd3a51111f13373bf6da3e00eea7068ff
Arunbhaarathi Anbu <anbu.a@northeastern.edu>
2024-11-12 21:47:28 -0500

    code cleanup

--- a_star.py
+++ a_star.py
@@ -61,8 +61,6 @@ def create_grid(rows,cols,node_size):
         for col in range(cols):
             node=cell((row,col))
             grid[row].append(node)
-            # pygame.draw.rect(screen, (0,0,0), (col * node_size, row * node_size, node_size, node_size), 1)
-
     return grid
 
 def draw_grid(rows,cols,grid,node_size):
@@ -83,11 +81,8 @@ def reconstruct_path(start,goal):
 
     curr_node=goal
     while curr_node!=start:
-        # path.append(curr_node)
-        
         curr_node=curr_node.parent
         curr_node.set_color(BLUE)
-    # return path[::-1]
 
 def grid_reset(rows,cols,grid,start,goal):
     for row in range(rows):
@@ -101,27 +96,22 @@ def grid_reset(rows,cols,grid,start,goal):
 
 
 def astar(start,goal,grid,rows,node_size):
-    # compute_heuristics(grid,rows,start,goal)
 
     open_list=PriorityQueue()
     open_list_tracker=set()
     counter=itertools.count()
-    open_list.put((0,next(counter),start))#counter to tie-break nodes with same priority
+    open_list.put((0,next(counter),start)) #counter to tie-break nodes with same priority
     open_list_tracker.add(start)
     start.g=0
     closed_list=set()
-    # open_list.append(start)
-    # f=float("inf")
 
     while not open_list.empty():
         current_node=open_list.get()[2]
         open_list_tracker.remove(current_node)
-        # print("curretn nopde",current_node)
 
         if current_node==goal:
             reconstruct_path(start,goal)
             return
-
 
         neighbors=current_node.get_neighbors(grid,rows)
 
@@ -135,12 +125,10 @@ def astar(start,goal,grid,rows,node_size):
             node.calcualte_h(goal)
             if node not in open_list_tracker:
                 f=node.g+node.h
-                # print("f:",f)
                 open_list.put((f,next(counter),node))
                 open_list_tracker.add(node)
                 node.color=YELLOW
                 visualize_search(node.position[0],node.position[1],node_size,node.color)
-                # draw_grid(rows,rows,grid,node_size)
                 node.parent=current_node
         current_node.color=GRAY
         visualize_search(current_node.position[0],current_node.position[1],node_size,node.color)
@@ -152,15 +140,9 @@ def astar(start,goal,grid,rows,node_size):
 
 def main(win_size,rows):
     node_size=win_size//rows
-
-
     grid = create_grid(rows,rows,node_size)
-
     start_node=None
     goal_node=None
-    
-
-
     run=True
 
     while run:
@@ -201,10 +183,6 @@ def main(win_size,rows):
                     if color is None:
                         print(row,col)
                         sys.exit()
-                    # print((row,col,color))
-                    # pygame.draw.rect(screen,color,(col * node_size, row * node_size, node_size, node_size))
-                    # pygame.draw.rect(screen, (0,0,0), (col * node_size, row * node_size, node_size, node_size), 1)
-
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -223,8 +201,6 @@ main(WIN_SIZE,50)
 
 
 """THings to add: 
-    2) Visualize the graph traversal
-    3) cleanup code
     4) add docstrings
     5) crerate unittest
     """
